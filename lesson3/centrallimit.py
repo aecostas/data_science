d53de68dceb8d9c95dc7e01cbda4046a6572d0a9
Andrés Estévez <aecostas@gmail.com>
2017-02-26 18:12:58 +0100

    Cleanup

--- lesson3/centrallimit.py
+++ lesson3/centrallimit.py
@@ -39,67 +39,6 @@ def uniform(samples, iterations, low, high):
         print shapiro_test
     return values
 
-
-def central_limit_theorem_OLD(iterations):
-    samples = 1000;
-    values = np.zeros(samples);
-
-    # for i in range(iterations):
-    #     values += np.random.uniform(low=-1, high=1, size=samples)
-    #     print stats.shapiro(values)
-
-    # reference = np.random.poisson(2, samples)
-    # count, bins, ignored = plt.hist(reference, 100, normed=True)
-
-    shapiro_W = []
-    shapiro_p = []
-
-    for i in range(iterations):
-        tmp_W = []
-        tmp_p = []
-
-        for trial in range(100):
-            valuesForTrial = np.array(values)
-            valuesForTrial += np.random.uniform(low=-100, high=100, size=samples)
-            shapiro_test = stats.shapiro(valuesForTrial)
-            tmp_W.append(shapiro_test[0])
-            tmp_p.append(shapiro_test[1])
-
-        values = np.array(valuesForTrial)
-        shapiro_W.append(np.array(tmp_W).mean())
-        shapiro_p.append(np.array(tmp_p).mean())
-
-    count, bins, ignored = plt.hist(values, 20, normed=True)
-    plt.show()
-
-    # P-Value trendline
-    x = range(iterations)
-    y = shapiro_p
-    z = np.polyfit(x, y, 1)
-    p = np.poly1d(z)
-    fig = pylab.plot(x,p(x),'r--')
-
-    plt.plot(y, 'ro')
-    plt.title('Shapiro-Wilk test for central limit theorem evaluation')
-    plt.ylabel('p-value')
-    plt.xlabel('Number of random variables added')
-    plt.show();
-
-    # P-Value trendline
-    x = range(iterations)
-    y = shapiro_W
-    z = np.polyfit(x, y, 1)
-    p = np.poly1d(z)
-    fig = pylab.plot(x,p(x),'r--')
-
-    plt.plot(y, 'W')
-    plt.title('Shapiro-Wilk test for central limit theorem evaluation')
-    plt.ylabel('W')
-    plt.xlabel('Number of random variables added')
-    plt.show();
-
-#    plt.savefig(filename+'_p.png', facecolor='#BABABA' , edgecolor='none', dpi=1200)
-
 if __name__ == "__main__":
     uniformDependent = uniform_dependent(1000, 30, 100, 100)
     uniformIndependent = uniform(1000, 30, 100, 100)
